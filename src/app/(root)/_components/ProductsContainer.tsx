af6ca9539a96a2c86f3b2ee2c75cf0e6945a5a87
shafik720 <shafikrasel5@gmail.com>
2025-04-25 17:05:11 +0600

    feat: implement search functionality, type definitions, and fetching products

--- src/app/(root)/_components/ProductsContainer.tsx
+++ src/app/(root)/_components/ProductsContainer.tsx
@@ -1,10 +1,52 @@
-"use client" ; 
-import { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
+import { ApiResponse, Product } from "@/types/types";
 
 const ProductsContainer = () => {
-
+    const [products, setProducts] = useState<Product[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [sortBy, setSortBy] = useState("id");
+    const [sortOrder, setSortOrder] = useState("desc");
+    const [lastPage, setLastPage] = useState(1);
+
+
+    const fetchProducts = async () => {
+        try {
+            setLoading(true);
+            setError(null);
+            const queryParams = new URLSearchParams({
+                page: currentPage.toString(),
+                ...(searchTerm && { search: searchTerm }),
+                sort_by: sortBy,
+                order: sortOrder,
+            });
+
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/api/ProductList?${queryParams}`
+            );
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch products");
+            }
+
+            const data: ApiResponse = await response.json();
+            setProducts(data.data);
+            setLastPage(data.last_page);
+        } catch (error) {
+            setError("Error fetching products. Please try again later.");
+            console.error("Error fetching products:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(()=>{
+        console.log(fetchProducts());
+    },[products])
 
     return (
         <div className="container mx-auto px-4 py-8">
